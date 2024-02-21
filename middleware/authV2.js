a3e2613e662e19b34f1069971c3db3ede838337a
vinay <vinayaggarwal5050@gmail.com>
2024-02-22 01:17:19 +0530

    auth middleware version two created

---
+++ middleware/authV2.js
@@ -0,0 +1,49 @@
+const { verfityJwtTokenAndReturnUserEmailAndId } = require("../services/jwtAuth");
+
+
+  /*
+  //when front end stores the token in file and local storage and returns that same in header
+  //front end
+  fetch("", {
+    method: "post",
+
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${jwtToken}`
+    },
+
+    body: JSON.stringify({
+      title: "some title...",
+      description: "some description..."
+    })
+  })
+
+  */
+
+//Authentication middleware
+function checkForAuthentication(req, res, next) {
+
+  req.user = null;
+  const authorizationHeader = req.headers.Authorization;
+
+  if(!authorizationHeader || !authorizationHeader.startsWith('Bearer')) {
+    return next();
+  }
+
+  const jwtToken = authString.split('Bearer ')[1];
+
+  req.user = verfityJwtTokenAndReturnUserEmailAndId(jwtToken); //--> user = null, if invalid token, else user = {email: "", _id: ""}
+  return next();
+}
+
+//authorization middleware
+function restrictTo(roles = []) {
+  return function(req, res, next) {
+    if(!req.user) return res.redirect('/signin');
+
+    if(!roles.includes(req.user.role)) return res.end('you are unauthorized');
+
+    next();
+
+  }
+}
